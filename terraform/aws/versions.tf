6e6ccb408efe43c2d8ed1b22ec35f95688fb00b4
Carlos <cuococarlos@gmail.com>
2026-03-04 15:41:53 -0300

    agregar backend s3 para mantener terraform.tfstate

--- terraform/aws/versions.tf
+++ terraform/aws/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  backend "s3" {
+    bucket = "trenecitos-terraform-state-211125610418"
+    key    = "aws/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
